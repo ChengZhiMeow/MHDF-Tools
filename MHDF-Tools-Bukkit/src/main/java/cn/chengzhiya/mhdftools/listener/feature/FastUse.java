f0502360408f37a56ec1b20b1249a5330178cc77
白神遥真可爱 <292200693@qq.com>
2025-06-05 19:28:06 +0800

    移除打印

--- MHDF-Tools-Bukkit/src/main/java/cn/chengzhiya/mhdftools/listener/feature/FastUse.java
+++ MHDF-Tools-Bukkit/src/main/java/cn/chengzhiya/mhdftools/listener/feature/FastUse.java
@@ -22,7 +22,6 @@ public final class FastUse extends AbstractListener {
         Player player = event.getPlayer();
         ItemStack item = player.getInventory().getItemInMainHand();
 
-        System.out.println(event.getAction());
         if (event.getAction() != Action.RIGHT_CLICK_AIR) {
             return;
         }
