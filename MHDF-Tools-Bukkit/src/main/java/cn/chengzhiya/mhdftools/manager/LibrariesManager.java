db99810f8483a8f8751c465b2d5bf0e3571070e6
橙汁喵 <292200693@qq.com>
2025-07-17 19:33:53 +0800

    更新packetevents版本至2.9.1

--- MHDF-Tools-Bukkit/src/main/java/cn/chengzhiya/mhdftools/manager/LibrariesManager.java
+++ MHDF-Tools-Bukkit/src/main/java/cn/chengzhiya/mhdftools/manager/LibrariesManager.java
@@ -76,7 +76,7 @@ public final class LibrariesManager {
             mhdfLibrary.addDependencyConfig(new DependencyConfig(
                     handleString("com{}github{}retrooper"),
                     "packetevents-api",
-                    "2.8.0",
+                    "2.9.1",
                     codemc,
                     new RelocateConfig(true, true,
                             handleString("io{}github{}retrooper")
@@ -85,7 +85,7 @@ public final class LibrariesManager {
             mhdfLibrary.addDependencyConfig(new DependencyConfig(
                     handleString("com{}github{}retrooper"),
                     "packetevents-netty-common",
-                    "2.8.0",
+                    "2.9.1",
                     codemc,
                     new RelocateConfig(true, true,
                             handleString("io{}github{}retrooper")
@@ -94,7 +94,7 @@ public final class LibrariesManager {
             mhdfLibrary.addDependencyConfig(new DependencyConfig(
                     handleString("com{}github{}retrooper"),
                     "packetevents-spigot",
-                    "2.8.0",
+                    "2.9.1",
                     codemc,
                     new RelocateConfig(true, true,
                             handleString("io{}github{}retrooper")
