fa17e7edd105880ebe948a1c5754a420060c9636
橙汁喵 <292200693@qq.com>
2025-08-02 02:55:12 +0800

    整理代码

--- MHDF-Tools-Bukkit/src/main/java/cn/chengzhiya/mhdftools/util/feature/ChatUtil.java
+++ MHDF-Tools-Bukkit/src/main/java/cn/chengzhiya/mhdftools/util/feature/ChatUtil.java
@@ -36,50 +36,32 @@ public final class ChatUtil {
      */
     public static TextComponent applyReplaceWord(CommandSender player, TextComponent messageComponent, String message) {
         ConfigurationSection config = Main.instance.getConfigManager().getConfigManager().getData().getConfigurationSection("chatSettings.replaceWord");
-        if (config == null) return messageComponent;
-        if (!config.getBoolean("enable")) return messageComponent;
+        if (config == null || !config.getBoolean("enable")) return messageComponent;
 
-        List<YamlConfiguration> replaceList = Main.instance.getConfigManager().getConfigManager().getYamlConfigurationList("chatSettings.replaceWord.replace");
-        for (YamlConfiguration replace : replaceList) {
+        for (YamlConfiguration replace : Main.instance.getConfigManager().getConfigManager().getYamlConfigurationList("chatSettings.replaceWord.replace")) {
             String type = replace.getString("type");
             if (type == null) return messageComponent;
-
-            String permission = replace.getString("bypass.permission");
-            if (replace.getBoolean("bypass.enable")) {
-                if (permission == null) continue;
-                if (player.hasPermission(permission)) continue;
-            }
+            if (replace.getBoolean("bypass.enable") && player.hasPermission(replace.getString("bypass.permission", ""))) continue;
 
             boolean regex = replace.getBoolean("regex");
             for (String s : replace.getStringList("word")) {
                 if (!message.contains(s)) continue;
-
                 switch (type) {
                     case "line" -> {
                         List<String> lineList = replace.getStringList("lineList");
-                        if (lineList.isEmpty()) break;
-
-                        s = message;
-                        messageComponent = ColorUtil.color(lineList.get(new Random().nextInt(lineList.size())));
+                        if (!lineList.isEmpty()) messageComponent = ColorUtil.color(lineList.get(new Random().nextInt(lineList.size())));
                     }
                     case "word" -> {
                         String word = replace.getString("replaceWord");
-                        if (word == null) break;
-
-                        messageComponent = messageComponent.replace(s, word);
+                        if (word != null) messageComponent = messageComponent.replace(s, word);
                     }
                 }
-
                 if (regex) {
-                    Pattern pattern = Pattern.compile(s);
-                    Matcher matcher = pattern.matcher(message);
-                    if (!matcher.find()) continue;
-
-                    messageComponent = messageComponent.replace("{value}", matcher.group());
+                    Matcher matcher = Pattern.compile(s).matcher(message);
+                    if (matcher.find()) messageComponent = messageComponent.replace("{value}", matcher.group());
                 }
             }
         }
-
         return messageComponent;
     }
 
@@ -92,25 +74,19 @@ public final class ChatUtil {
      */
     public static TextComponent applyShowItem(Player player, TextComponent messageComponent) {
         ConfigurationSection config = Main.instance.getConfigManager().getConfigManager().getData().getConfigurationSection("chatSettings.showItem");
-        if (config == null) return messageComponent;
-        if (!config.getBoolean("enable")) return messageComponent;
+        if (config == null || !config.getBoolean("enable")) return messageComponent;
 
         ItemStack item = player.getInventory().getItemInMainHand();
-        ItemMeta meta = item.getItemMeta();
-        Material itemType = item.getType();
-
-        if (itemType.isAir()) return messageComponent;
-
-        UUID uuid = UUID.randomUUID();
-        Main.instance.getCacheManager().put("showItem", uuid.toString(), Base64Util.encode(item.serializeAsBytes()));
-        MHDFScheduler.getAsyncScheduler().runTaskLater(Main.instance, () ->
-                        Main.instance.getCacheManager().remove("showItem", uuid.toString()),
-                20L * config.getInt("removeCache")
-        );
+        if (item.getType().isAir()) return messageComponent;
 
         String format = config.getString("format");
         if (format == null) return messageComponent;
 
+        UUID uuid = UUID.randomUUID();
+        Main.instance.getCacheManager().put("showItem", uuid.toString(), Base64Util.encode(item.serializeAsBytes()));
+        MHDFScheduler.getAsyncScheduler().runTaskLater(Main.instance, () -> Main.instance.getCacheManager().remove("showItem", uuid.toString()), 20L * config.getInt("removeCache"));
+
+        ItemMeta meta = item.getItemMeta();
         Component displayName = PluginUtil.isNativeSupportAdventureApi() && meta.hasCustomName() ? meta.customName() : ColorUtil.color(Main.instance.getMinecraftLangManager().getItemName(item));
         TextComponent formatComponent = ColorUtil.color(format)
                 .replace("{uuid}", uuid.toString())
@@ -118,19 +94,8 @@ public final class ChatUtil {
                 .replace("{amount}", String.valueOf(item.getAmount()));
 
         for (String s : config.getStringList("word")) {
-            messageComponent = messageComponent.replace(
-                    s,
-                    formatComponent.hoverEvent(item.asHoverEvent())
-            );
+            messageComponent = messageComponent.replace(s, formatComponent.hoverEvent(item.asHoverEvent()));
         }
-
-        for (String s : config.getStringList("word")) {
-            messageComponent = messageComponent.replace(
-                    s,
-                    formatComponent.hoverEvent(item.asHoverEvent())
-            );
-        }
-
         return messageComponent;
     }
 
@@ -142,41 +107,7 @@ public final class ChatUtil {
      * @return 处理后的文本
      */
     public static TextComponent applyShowInventory(Player player, TextComponent messageComponent) {
-        ConfigurationSection config = Main.instance.getConfigManager().getConfigManager().getData().getConfigurationSection("chatSettings.showInventory");
-
-        if (config == null) return messageComponent;
-        if (!config.getBoolean("enable")) return messageComponent;
-
-        ItemStack[] contents = player.getInventory().getContents();
-        JSONObject inventoryData = new JSONObject();
-        for (int i = 0; i < contents.length; i++) {
-            ItemStack item = contents[i];
-            if (item == null || item.getType() == Material.AIR || item.getAmount() <= 0) continue;
-
-            inventoryData.put(String.valueOf(i), Base64Util.encode(item.serializeAsBytes()));
-        }
-
-        UUID uuid = UUID.randomUUID();
-        Main.instance.getCacheManager().put("showInventory", uuid.toString(), inventoryData.toString());
-
-        MHDFScheduler.getAsyncScheduler().runTaskLater(Main.instance, () ->
-                        Main.instance.getCacheManager().remove("showInventory", uuid.toString()),
-                20L * config.getInt("removeCache")
-        );
-
-        String format = config.getString("format");
-        if (format == null) {
-            return messageComponent;
-        }
-        format = format
-                .replace("{uuid}", uuid.toString())
-                .replace("{player}", MHDFToolsAPIHelper.getInstance().getPlayerManager().getPlayer(player).getDisplayName());
-
-        for (String s : config.getStringList("word")) {
-            messageComponent = messageComponent.replace(s, ColorUtil.color(format));
-        }
-
-        return messageComponent;
+        return applyShowableContainer(player, messageComponent, "showInventory", player.getInventory().getContents());
     }
 
     /**
@@ -187,45 +118,7 @@ public final class ChatUtil {
      * @return 处理后的文本
      */
     public static TextComponent applyShowEnderChest(Player player, TextComponent messageComponent) {
-        ConfigurationSection config = Main.instance.getConfigManager().getConfigManager().getData().getConfigurationSection("chatSettings.showEnderChest");
-        if (config == null) {
-            return messageComponent;
-        }
-
-        if (!config.getBoolean("enable")) {
-            return messageComponent;
-        }
-
-        ItemStack[] contents = player.getEnderChest().getContents();
-        JSONObject inventoryData = new JSONObject();
-        for (int i = 0; i < contents.length; i++) {
-            ItemStack item = contents[i];
-            if (item == null || item.getType() == Material.AIR || item.getAmount() <= 0) continue;
-
-            inventoryData.put(String.valueOf(i), Base64Util.encode(item.serializeAsBytes()));
-        }
-
-        UUID uuid = UUID.randomUUID();
-        Main.instance.getCacheManager().put("showEnderChest", uuid.toString(), inventoryData.toString());
-
-        MHDFScheduler.getAsyncScheduler().runTaskLater(Main.instance, () ->
-                        Main.instance.getCacheManager().remove("showEnderChest", uuid.toString()),
-                20L * config.getInt("removeCache")
-        );
-
-        String format = config.getString("format");
-        if (format == null) {
-            return messageComponent;
-        }
-        format = format
-                .replace("{uuid}", uuid.toString())
-                .replace("{player}", MHDFToolsAPIHelper.getInstance().getPlayerManager().getPlayer(player).getDisplayName());
-
-        for (String s : config.getStringList("word")) {
-            messageComponent = messageComponent.replace(s, ColorUtil.color(format));
-        }
-
-        return messageComponent;
+        return applyShowableContainer(player, messageComponent, "showEnderChest", player.getEnderChest().getContents());
     }
 
     /**
@@ -238,44 +131,28 @@ public final class ChatUtil {
      */
     public static TextComponent applyAt(TextComponent messageComponent, String message, Set<String> atList) {
         ConfigurationSection config = Main.instance.getConfigManager().getConfigManager().getData().getConfigurationSection("chatSettings.at");
-        if (config == null) {
-            return messageComponent;
-        }
+        if (config == null) return messageComponent;
 
         String patternFormat = config.getString("patternFormat");
-        if (patternFormat == null) {
-            return messageComponent;
-        }
+        if (patternFormat == null) return messageComponent;
 
         TextComponent format = Main.instance.getConfigManager().getLangManager().i18n("chat.at.format");
         for (String at : atList) {
-            Pattern pattern = Pattern.compile(patternFormat.replace("{at}", at));
-            Matcher matcher = pattern.matcher(message);
-
+            Matcher matcher = Pattern.compile(patternFormat.replace("{at}", at)).matcher(message);
             if (matcher.find()) {
-                messageComponent = messageComponent
-                        .replace(matcher.group(), format.replace("{name}", at));
+                messageComponent = messageComponent.replace(matcher.group(), format.replace("{name}", at));
             }
         }
 
         if (atList.contains(AtUtil.getAtAll())) {
+            TextComponent allFormat = format.replace("{name}", Main.instance.getConfigManager().getLangManager().i18n("chat.at.all"));
             for (String at : config.getStringList("allMessage")) {
-                Pattern pattern = Pattern.compile(patternFormat.replace("{at}", at));
-                Matcher matcher = pattern.matcher(message);
-
+                Matcher matcher = Pattern.compile(patternFormat.replace("{at}", at)).matcher(message);
                 if (matcher.find()) {
-                    messageComponent = messageComponent
-                            .replace(
-                                    matcher.group(),
-                                    format.replace(
-                                            "{name}",
-                                            Main.instance.getConfigManager().getLangManager().i18n("chat.at.all")
-                                    )
-                            );
+                    messageComponent = messageComponent.replace(matcher.group(), allFormat);
                 }
             }
         }
-
         return messageComponent;
     }
 
@@ -289,16 +166,50 @@ public final class ChatUtil {
     public static TextComponent formatMessage(Player player, TextComponent message) {
         ConfigurationSection config = Main.instance.getConfigManager().getConfigManager().getData().getConfigurationSection("chatSettings.format");
         String group = GroupUtil.getGroup(player, config, "mhdftools.group.chatformat.");
-
-        String format;
-        if (config == null || !config.getBoolean("enable")) {
-            format = "<{player}> {message}";
-        } else {
-            format = config.getString(group + ".format");
-        }
+        String format = (config == null || !config.getBoolean("enable")) ? "<{player}> {message}" : config.getString(group + ".format");
 
         return ColorUtil.color(Main.instance.getPluginHookManager().getPlaceholderAPIHook().placeholder(player, format))
                 .replace("{player}", MHDFToolsAPIHelper.getInstance().getPlayerManager().getPlayer(player).getDisplayName())
                 .replace("{message}", message);
+    }
+
+    /**
+     * 处理展示容器后的文本
+     *
+     * @param player           玩家实例
+     * @param messageComponent 文本实例
+     * @param configKey        配置名称
+     * @param contents         物品数组
+     * @return 处理后的文本
+     */
+    private static TextComponent applyShowableContainer(Player player, TextComponent messageComponent, String configKey, ItemStack[] contents) {
+        ConfigurationSection config = Main.instance.getConfigManager().getConfigManager().getData().getConfigurationSection("chatSettings." + configKey);
+        if (config == null || !config.getBoolean("enable")) return messageComponent;
+
+        String format = config.getString("format");
+        if (format == null) return messageComponent;
+
+        JSONObject inventoryData = new JSONObject();
+        for (int i = 0; i < contents.length; i++) {
+            ItemStack item = contents[i];
+            if (item != null && !item.getType().isAir()) {
+                inventoryData.put(String.valueOf(i), Base64Util.encode(item.serializeAsBytes()));
+            }
+        }
+
+        if (inventoryData.isEmpty()) return messageComponent;
+
+        UUID uuid = UUID.randomUUID();
+        Main.instance.getCacheManager().put(configKey, uuid.toString(), inventoryData.toString());
+        MHDFScheduler.getAsyncScheduler().runTaskLater(Main.instance, () -> Main.instance.getCacheManager().remove(configKey, uuid.toString()), 20L * config.getInt("removeCache"));
+
+        TextComponent formatComponent = ColorUtil.color(format
+                .replace("{uuid}", uuid.toString())
+                .replace("{player}", MHDFToolsAPIHelper.getInstance().getPlayerManager().getPlayer(player).getDisplayName()));
+
+        for (String s : config.getStringList("word")) {
+            messageComponent = messageComponent.replace(s, formatComponent);
+        }
+        return messageComponent;
     }
 }
